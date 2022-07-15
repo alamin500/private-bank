9b920d64e123e0fdca67edfbce80f35e988233d2
Mohammad Alamin <86641758+alamin500@users.noreply.github.com>
2022-07-15 09:31:41 +0600

    changes

--- js/bank.js
+++ js/bank.js
@@ -1,18 +1,3 @@
-// document.getElementById('login-submit').addEventListener('click', function(){
-//     const emailField = document.getElementById('user-email');
-//     const userEmail = emailField.value;
-
-//     // get user password
-//     const passwordField = document.getElementById('user-password');
-//     const userPassword = passwordField.value;
-
-//     if(userEmail == 'sontan@gmail.com' && userPassword == 'secret'){
-//         window.location.href = 'banking.html'
-
-//     }
-
-// })
-
 document.getElementById("login-submit").addEventListener("click", function () {
   const userEmail = document.getElementById("user-email").value;
   const userPassword = document.getElementById("user-password").value;
@@ -20,6 +5,6 @@ document.getElementById("login-submit").addEventListener("click", function () {
   if (userEmail === "alamin@gmail.com" && userPassword === "alamin") {
     window.location.href = "banking.html";
   } else {
-    console.log("Password is incorrect");
+    alert("Email/Password is incorrect");
   }
 });
